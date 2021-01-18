98b09284c88cec253386ccf7e42317e5841a5110
yabby <yabby1997@gmail.com>
2021-01-18 21:40:46 +0900

    [init] first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by yabby on 2021/01/18.
+//
+
+import SwiftUI
+
+@main   //app으로의 entry point
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
